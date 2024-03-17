207dc07018735cf4f059682e09a3fabf68474725
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2024-03-18 00:39:57 +0100

    new task

--- flows/main.py
+++ flows/main.py
@@ -1,4 +1,5 @@
 from tasks.adders import print_t2
+from tasks.tracker import see_behavior
 from prefect import flow
 
 @flow(
@@ -7,6 +8,7 @@ log_prints=True
 def run_demo():
     print("Helll-oooo")
     print_t2()
+    see_behavior()
 
 if __name__ == "__main__":
     run_demo()
---
+++ tasks/tracker.py
@@ -0,0 +1,6 @@
+from prefect import task
+
+
+@task
+def see_behavior():
+    print("Just trying to understand how update to code works!")
